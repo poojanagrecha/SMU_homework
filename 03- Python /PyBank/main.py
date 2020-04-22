c7c41634cce464cac0ce27b3452627d14c5b8966
Pooja Nagrecha <poojanagrecha@poojas-air-2.attlocal.net>
2020-04-21 23:45:38 -0500

    hw

---
+++ 03- Python /PyBank/main.py
@@ -0,0 +1,64 @@
+import csv
+csvpath = r"SMU stuff/smu-dal-data-pt-03-2020-u-c/02-Homework/03-Python/Instructions/PyBank/Resources/budget_data.csv"
+
+totalMonths = 0
+totalProfit = 0
+profitChanges = []
+lastProfit = 0
+currentProfit = 0
+rowCount = 0
+greatestIncrease = 0 
+
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    #print(csvreader)
+
+    csv_header = next(csvreader)
+    #print(f"CSV Header: {csv_header}")
+
+    for row in csvreader: 
+        #print(row)
+        totalMonths += 1 
+         
+        totalProfit += int(row[1])
+        
+
+        if rowCount == 0:
+            lastProfit = int(row[1])
+            greatestIncreasemonth = (row[0])
+            greatestDecreasemonth = (row[0])
+            greatestIncrease = 0
+            greatestDecrease = 0
+        else:
+            currentProfit = int(row[1])
+            difference = currentProfit - lastProfit
+            profitChanges.append(difference)
+            lastProfit = currentProfit
+            if greatestIncrease > difference:
+                pass
+            else: 
+                greatestIncrease = difference
+                greatestIncreasemonth = (row[0])
+            if greatestDecrease < difference:
+                pass
+            else: 
+                greatestDecrease = difference
+                greatestDecreasemonth = (row[0])
+
+
+        
+        rowCount += 1 
+    print(profitChanges)
+    average = sum(profitChanges)/ len(profitChanges)
+    greatestIncrease = max(profitChanges)
+
+  
+
+    
+    print("Total Months:" + str(totalMonths))
+    print('Total Profit:' + str(totalProfit))
+    print('Average Change:'+ str(average))
+    print('Greatest Increase in Profits ' + str(greatestIncreasemonth) + " " + str(greatestIncrease))
+    print('Greatest Decrease in Profits ' + str(greatestDecreasemonth) + " " + str(greatestDecrease))
+
+    python /path/to/script/myscript.py > /path/to/output/myfile.txt
