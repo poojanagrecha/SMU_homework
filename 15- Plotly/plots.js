11ba3ed1ca1b12b990f7f0f6f3ad72e24c72f225
Pooja Nagrecha <poojanagrecha@yahoo.com>
2020-09-14 14:26:11 -0500

    hw

--- 15- Plotly/plots.js
+++ 15- Plotly/plots.js
@@ -1,109 +1,100 @@
-function makeChart(id) {
-    //Read samples.json
-    d3.json("samples.json").then(sampledata => {
-        console.log(sampledata)
+function buildMetadata(sample) {
+    d3.json("samples.json").then((data) => {
+        var metadata = data.metadata;
+        var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+        var result = resultArray[0];
 
-        var ids = sampledata.samples[0].otu_ids;
-        console.log(ids)
-        var sampleValues = sampledata.samples[0].sample_values.slice(0, 10).reverse();
-        console.log(sampleValues)
-        var labels = sampledata.samples[0].otu_labels.slice(0, 10);
-        console.log(labels)
-        var OTU_top = (sampledata.samples[0].otu_ids.slice(0, 10)).reverse();
-        var OTU_id = OTU_top.map(d => "OTU " + d);
-        console.log(`OTU IDS: ${OTU_id}`)
-        var labels = sampledata.samples[0].otu_labels.slice(0, 10);
-        console.log(`OTU_labels: ${labels}`)
+        var PANEL = d3.select("#sample-metadata");
 
-        //    bar graph
-        var trace = {
-            x: sampleValues,
-            y: OTU_id,
-            text: labels,
+
+        PANEL.html("");
+
+        Object.entries(result).forEach(([key, value]) => {
+            PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+        });
+    });
+}
+
+function buildCharts(sample) {
+    d3.json("samples.json").then((data) => {
+        var samples = data.samples;
+        var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
+        var result = resultArray[0];
+
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+
+        // bubble chart
+        var bubbleLayout = {
+            title: "Bacteria Cultures Per Sample",
+            margin: { t: 0 },
+            hovermode: "closest",
+            xaxis: { title: "OTU ID" },
+            margin: { t: 30 }
+        };
+        var bubbleData = [{
+            x: otu_ids,
+            y: sample_values,
+            text: otu_labels,
+            mode: "markers",
             marker: {
-                color: 'purple'
+                size: sample_values,
+                color: otu_ids,
+                colorscale: "Pink"
+            }
+        }];
+
+        Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+
+        var yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
+        var barData = [{
+            y: yticks,
+            x: sample_values.slice(0, 10).reverse(),
+            text: otu_labels.slice(0, 10).reverse(),
+            marker: {
+                color: "purple"
             },
             type: "bar",
             orientation: "h",
-        };
-        // create data variable
-        var data = [trace];
+        }];
 
-        var layout = {
-            title: "Top 10 OTU",
-            yaxis: {
-                tickmode: "linear",
-            },
-            margin: {
-                l: 100,
-                r: 100,
-                t: 100,
-                b: 30
-            }
-        };
-        Plotly.newPlot("bar", data, layout);
-
-        // Bubble graph
-        var trace1 = {
-            x: sampledata.samples[0].otu_ids,
-            y: sampledata.samples[0].sample_values,
-            mode: "markers",
-            marker: {
-                size: sampledata.samples[0].sample_values,
-                color: sampledata.samples[0].otu_ids
-            },
-            text: sampledata.samples[0].otu_labels
-
+        var barLayout = {
+            title: "Top 10 Bacteria Cultures Found",
+            margin: { t: 30, l: 150 }
         };
 
-        // layout
-        var layout_2 = {
-            xaxis: { title: "OTU ID" },
-            height: 600,
-            width: 1000
-        };
-        var data1 = [trace1];
-
-        Plotly.newPlot("bubble", data1, layout_2);
-
+        Plotly.newPlot("bar", barData, barLayout);
     });
-}
-
-function getDemoInfo(id) {
-    d3.json("samples.json").then((data) => {
-        var metadata = data.metadata;
-        console.log(metadata)
-
-
-        var result = metadata.filter(meta => meta.id.toString() === id)[0];
-        var demographicInfo = d3.select("#sample-metadata");
-        demographicInfo.html("");
-
-        Object.entries(result).forEach((key) => {
-            demographicInfo.append("h5").text(key[0].toUpperCase() + ": " + key[1] + "\n");
-        });
-    });
-}
-// change event
-function optionChanged(id) {
-    makeChart(id);
-    getDemoInfo(id);
 }
 
 function init() {
-    // dropdown menu
-    var dropdown = d3.select("#selDataset");
+    // Grab a reference to the dropdown select element
+    var selector = d3.select("#selDataset");
 
+    // Use the list of sample names to populate the select options
     d3.json("samples.json").then((data) => {
-        console.log(data)
+        var sampleNames = data.names;
 
-        data.names.forEach(function(name) {
-            dropdown.append("option").text(name).property("value");
+        sampleNames.forEach((sample) => {
+            selector
+                .append("option")
+                .text(sample)
+                .property("value", sample);
         });
 
-        makeChart(data.names[0]);
-        getDemoInfo(data.names[0]);
+        // Use the first sample from the list to build the initial plots
+        var firstSample = sampleNames[0];
+        buildCharts(firstSample);
+        buildMetadata(firstSample);
     });
 }
 
+function optionChanged(newSample) {
+    // Fetch new data each time a new sample is selected
+    buildCharts(newSample);
+    buildMetadata(newSample);
+}
+
+// Initialize the dashboard
 init();
